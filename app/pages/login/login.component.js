4621ad30220591aa64c80be7cb6d3ac0e46379b6
russunit <russunit@gmail.com>
2016-11-07 22:27:57 -0500

    new "pages" folder
    deleted contents of the pages folder, added 8 folders representing each
    of our 8 views: dashboard, charter, shuttle, fbo, mro, passport jet,
    menu, and login/signup.

--- app/pages/login/login.component.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibG9naW4uY29tcG9uZW50LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibG9naW4uY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
