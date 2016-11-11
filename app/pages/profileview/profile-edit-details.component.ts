4511d50931f9631677f4e6581f842121f40feae5
russunit <russunit@gmail.com>
2016-11-11 14:51:25 -0500

    imported all components to app.module.ts, eliminated profile-edit-details component
    the functions of profile-edit-details will be moved to profile-settings

--- app/pages/profileview/profile-edit-details.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "profile-edit-details",
-  template: "<Label text='Profile Edit Details Component'></Label>"
-})
-export class ProfileEditDetailsComponent {}
