6cbe930c87128b460499f288ab3af47dc464a721
russunit <russunit@gmail.com>
2016-11-11 17:41:03 -0500

    fixing file names
    let's stick to the naming convention, lower case.

--- app/pages/shuttle/shuttle.component.ts
+++ app/pages/shuttle/shuttle.component.ts
@@ -1,7 +1,17 @@
 import { Component } from "@angular/core";
+import { ShuttleRequest } from './shuttle-request';
 
 @Component({
   selector: "shuttle",
-  template: "<Label text='Shuttle Comonent'></Label>"
+  template: "<Label text='Shuttle Component'></Label>"
+  //templateUrl: 'shuttle-component.html'
 })
-export class ShuttleComponent {}
+export class ShuttleComponent {
+	
+	sendRequest(request:ShuttleRequest)
+	{
+
+	}
+}
+
+//imports
