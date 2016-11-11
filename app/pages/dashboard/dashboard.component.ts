503afd3d3b4f4067b3f1a8c9ebd3c72eb4472386
russunit <russunit@gmail.com>
2016-11-11 18:00:05 -0500

    fixed syntax error

--- app/pages/dashboard/dashboard.component.ts
+++ app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import ( Router NavigationStart, NavigationEnd ) from "@angular/core";
+import { Router NavigationStart, NavigationEnd } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 //import { routes } from "./app.routes";
 
