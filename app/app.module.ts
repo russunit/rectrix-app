ae750008002710100a7f6c797a1b4a83416e2e64
russunit <russunit@gmail.com>
2016-11-11 17:07:31 -0500

    Revert "imported all components to app.module.ts, eliminated profile-edit-details component"
    This reverts commit 4511d50931f9631677f4e6581f842121f40feae5.

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,37 +4,7 @@ import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { CharterComponent } from './pages/charter/charter.component';
-import { ShuttleComponent } from './pages/shuttle/shuttle.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { FboComponent } from './pages/fbo/fbo.component';
-import { FboDetailsComponent } from './pages/fbo/fbo-details.component';
-import { LoginComponent } from './pages/login/login.component';
-import { MenuComponent } from './pages/menu/menu.component';
-import { MroComponent } from './pages/mro/mro.component';
-import { MroDetailsComponent } from './pages/mro/mro-details.component';
-import { NavbarComponent } from './pages/navbar/navbar.component';
-import { PassportJetComponent } from './pages/passportjet/passportjet.component';
-import { PassportJetJoinComponent } from './pages/passportjet/passportjet-join.component';
-import { ProfilebarComponent } from './pages/profilebar/profilebar.component';
-import { SignupComponent } from './pages/signup/signup.component';
-
-
-
-import { ProfileViewComponent } from './pages/profileview/profileview.component';
-import { ProfileShuttleHistoryComponent } from './pages/profileview/profile-shuttle-history.component';
-import { ProfileShuttleDetailsComponent } from './pages/profileview/profile-shuttle-details.component';
-import { ProfileCharterHistoryComponent } from './pages/profileview/profile-charter-history.component';
-import { ProfileCharterDetailsComponent } from './pages/profileview/profile-charter-details.component';
-import { ProfileSettingsComponent } from './pages/profileview/profile-settings.component';
-
-
-
-
-
-
-
+import { AppComponent } from "./app.component";
 
 @NgModule({
   imports: [NativeScriptModule],
---
+++ app/pages/profileview/profile-edit-details.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "profile-edit-details",
+  template: "<Label text='Profile Edit Details Component'></Label>"
+})
+export class ProfileEditDetailsComponent {}
