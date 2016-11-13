fbed124ce39cf408480990751283fe81f991c16b
russunit <russunit@gmail.com>
2016-11-12 20:20:12 -0500

    got dashboard and menu routing properly.
    now we have a real skeleton. time to make the components do something

--- app/pages/dashboard/dashboard.component.ts
+++ app/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,21 @@ import { Component } from "@angular/core";
 //import { Router, NavigationStart, NavigationEnd } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 //import { routes } from "./app.routes";
+import { Router } from "@angular/router";
+import { Location } from '@angular/common';
 
 @Component({
   selector: "dashboard",
-  template: "<Label text='Dashboard Component'></Label>"
+  template: `
+  	<GridLayout rows="auto, auto, auto" columns="auto, auto" horizontalAlignment="center" verticalAlignment="center">
+   		<button text="Shuttle" row="0" col="0" 		(tap)="goShuttle()"></button>
+   		<button text="Charter" row="0" col="1" 		(tap)="goCharter()"></button>
+   		<button text="FBOs" row="1" col="0" 		(tap)="goFbo()"></button>
+   		<button text="MROs" row="1" col="1" 		(tap)="goMro()"></button>
+   		<button text="Passport Jet" row="2" col="0" (tap)="goPassJet()"></button>
+   		<button text="Menu" row="2" col="1" 		(tap)="goMenu()"></button>
+	</GridLayout>
+  `
   
 //moved the buttons to dashboard.component.html
 
@@ -27,7 +38,27 @@ if(option=='Shuttle')
 {}//go to shuttle page
 if(option=='Charter flights')
 {}//go to charter page
-
 }
+
+constructor(private router: Router, private location: Location)
+{}
+
+goShuttle()
+{ this.router.navigate(["/shuttle"]); }
+
+goCharter()
+{ this.router.navigate(["/charter"]); }
+
+goFbo()
+{ this.router.navigate(["/fbo"]); }
+
+goMro()
+{ this.router.navigate(["/mro"]); }
+
+goPassJet()
+{ this.router.navigate(["/passportjet"]); }
+
+goMenu()
+{ this.router.navigate(["/menu"]); }
 
 }
--- app/pages/menu/menu.component.ts
+++ app/pages/menu/menu.component.ts
@@ -1,7 +1,50 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { Location } from '@angular/common';
 
 @Component({
   selector: "menu",
-  template: "<Label text='Menu Component'></Label>"
+  template: `
+  	<StackLayout>
+  		<button text="Shuttle" (tap)="goShuttle()"></button>
+  		<button text="Charter" (tap)="goCharter()"></button>
+  		<button text="FBO" (tap)="goFbo()"></button>
+  		<button text="MRO" (tap)="goMro()"></button>
+  		<button text="Passport Jet" (tap)="goPassJet()"></button>
+  		<button text="Log in" (tap)="logIn()"></button>
+  		<button text="Sign up" (tap)="signUp()"></button>
+  		<button text="View Profile" (tap)="viewProfile()"></button>
+  	</StackLayout>
+  	`
+
 })
-export class MenuComponent {}
+export class MenuComponent 
+{
+	constructor(private router: Router, private location: Location)
+	{}
+
+	logIn()
+	{ this.router.navigate(["/login"]); }
+
+	signUp()
+	{ this.router.navigate(["/signup"]); }
+
+	goShuttle()
+	{ this.router.navigate(["/shuttle"]); }
+
+	goCharter()
+	{ this.router.navigate(["/charter"]); }
+
+	goFbo()
+	{ this.router.navigate(["/fbo"]); }
+
+	goMro()
+	{ this.router.navigate(["/mro"]); }
+
+	goPassJet()
+	{ this.router.navigate(["/passportjet"]); }
+
+	viewProfile()
+	{ this.router.navigate(["/profile-view"]); }
+
+}
