33944a8477403968d2bff83ef2c452d21ab787b4
russunit <russunit@gmail.com>
2016-11-16 21:39:19 -0500

    edited shuttle-request, added charter-request classes

--- app/pages/charter/charter-request.ts
+++ app/pages/charter/charter-request.ts
@@ -1,2 +1,17 @@
 
-export class CharterRequest {}
+export class CharterRequest 
+{
+	firstName:String;
+lastName:String;
+phoneNumber:String;
+tripType:String;
+departLocation:String;
+departDate: String;
+departTime: String;
+arriveLocation: String;
+arriveDate: String;
+arriveTime: String;
+
+requirements: String;
+prefferedCraft: String;
+}
