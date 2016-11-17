86cf58bfc9838feff51e6da54fa115be7ddc28ea
russunit <russunit@gmail.com>
2016-11-16 21:36:28 -0500

    Added forms to Shuttle and Charter components

--- app/pages/charter/charter.component.ts
+++ app/pages/charter/charter.component.ts
@@ -6,13 +6,64 @@ import { CharterRequest } from './charter-request';
 ({
 
 selector: "charter",
-template: "<Label text='Charter Component'></Label>",
+template: `
+ <StackLayout>
+ <ScrollView>
+ <StackLayout>
+
+	<label text='Charter Component' horizontalAlignment='center'></label>
+
+	<label text='First Name'></label>
+	<TextField  autocapitalizationType="none"></TextField>
+
+	<label text='Last Name'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Trip Type'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Depart Location'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Depart Date'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Depart Time'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Arrive Location'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Arrive Date'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Arrive Time'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Requirements'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Preferred Craft'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<Button text="Request Charter" (tap)="sendRequest()" horizontalAlignment='center'></Button>
+
+
+ </StackLayout>
+ </ScrollView>
+ </StackLayout>
+
+
+
+
+	`,
 
 //moduleID
 //selector: "charter",
 //templateUrl: "charter-component.html",
 //styleUrls: "charter-component.css",
 //provider
+
 })
 
 export class CharterComponent
--- app/pages/shuttle/shuttle.component.ts
+++ app/pages/shuttle/shuttle.component.ts
@@ -3,7 +3,59 @@ import { ShuttleRequest } from './shuttle-request';
 
 @Component({
   selector: "shuttle",
-  template: "<Label text='Shuttle Component'></Label>"
+  template: `
+ <StackLayout>
+ <ScrollView>
+ <StackLayout>
+	<label text='Shuttle Component' horizontalAlignment='center'></label>
+
+	<label text='First Name'></label>
+	<TextField  autocapitalizationType="none"></TextField>
+
+	<label text='Last Name'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Trip Type'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Depart Location'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Depart Date'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Depart Time'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Arrive Location'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Arrive Date'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Arrive Time'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Number of Adults'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Number of Children'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<label text='Number of Infants'></label>
+	<TextField autocapitalizationType="none"></TextField>
+
+	<Button text="Request Shuttle" (tap)="sendRequest()" horizontalAlignment='center'></Button>
+
+
+ </StackLayout>
+ </ScrollView>
+ </StackLayout>
+
+
+
+
+	`,
   //templateUrl: 'shuttle-component.html'
 })
 export class ShuttleComponent {
