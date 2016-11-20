bf1ed9235430d35cf8a716be56216f7e949998fb
Anthony <anttoucan@hotmail.com>
2016-11-19 19:06:29 -0500

    Replace this with the version in node_modules/tns-core-modules

---
+++ weakmap.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+interface WeakMap<K, V> {
+    clear(): void;
+    delete(key: K): boolean;
+    get(key: K): V;
+    has(key: K): boolean;
+    set(key: K, value: V): WeakMap<K, V>;
+}
+
+declare var WeakMap: WeakMapConstructor;
