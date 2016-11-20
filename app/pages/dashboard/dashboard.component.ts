407c5c2145c2c82b2071f2bcf5d1ef36ace1d845
russunit <russunit@gmail.com>
2016-11-20 17:44:03 -0500

    fixed a few typos so it would run for me, plus changed "passenger jet" back to "passport jet"
    2 things:
    - make sure to keep track of upper/lower case in html elements (<label>,
    not <Label>, and <TextField>, not <Textfield>
    - "Passport Jet" is a membership program offered by rectrix, check it
    out on the site. There's no "passenger jet" option

--- app/pages/dashboard/dashboard.component.ts
+++ app/pages/dashboard/dashboard.component.ts
@@ -13,7 +13,7 @@ import { Location } from '@angular/common';
    		<button text="Charter" row="0" col="1" 		(tap)="goCharter()" height="80"></button>
    		<button text="FBO" row="1" col="0" 		(tap)="goFbo()" height="80"></button>
    		<button text="MRO" row="1" col="1" 		(tap)="goMro()" height="80"></button>
-   		<button text="Passenger Jet" row="2" col="0" (tap)="goPassJet()" height="80"></button>
+   		<button text="Passport Jet" row="2" col="0" (tap)="goPassJet()" height="80"></button>
    		<button text="Menu" row="2" col="1" 		(tap)="goMenu()" height="80"></button> 
 	</GridLayout>
   `
--- app/pages/signup/signup.component.ts
+++ app/pages/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { User } from './user';
 import { Router } from "@angular/router";
+import { TextField } from "ui/text-field";
+
 @Component({
   selector: "signup",
   template: `
@@ -10,28 +12,28 @@ import { Router } from "@angular/router";
 <label text='Signup Component'></label>
 
 <label text='First Name'></label>
-<Textfield autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.firstName"></TextField>
+<TextField autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.firstName"></TextField>
 
-<label text='Last Name'></Label>
-<Textfield autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.lastName"></TextField>
+<label text='Last Name'></label>
+<TextField autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.lastName"></TextField>
 
-<label text='Address'></Label>
-<Textfield autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.address"></TextField>
+<label text='Address'></label>
+<TextField autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.address"></TextField>
 
-<label text='City'></Label>
-<Textfield autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.city"></TextField>
+<label text='City'></label>
+<TextField autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.city"></TextField>
 
-<label text='Country'></Label>
-<Textfield autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.country"></TextField>
+<label text='Country'></label>
+<TextField autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.country"></TextField>
 
-<label text='Zip code'></Label>
-<Textfield keyboardType ="number" [(ngModel)]="user.zip"></TextField>
+<label text='Zip code'></label>
+<TextField keyboardType ="number" [(ngModel)]="user.zip"></TextField>
 
-<label text='Username'></Label>
-<Textfield autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.username"></TextField>
+<label text='Username'></label>
+<TextField autocorrect="false" autocapitalizationType="none" [(ngModel)]="user.username"></TextField>
 
-<label text='Password'></Label>
-<Textfield secure = "true" [(ngModel)]="user.password"></TextField>
+<label text='Password'></label>
+<TextField secure = "true" [(ngModel)]="user.password"></TextField>
 <Button [text]="Submit"(tap)="signup(this.user)"></Button>
 
  </StackLayout>
@@ -39,15 +41,18 @@ import { Router } from "@angular/router";
  </StackLayout>
 `,
 })
-export class SignupComponent {
+export class SignupComponent 
+{
 user : User;
 
 constructor(private router: Router)
 {
        this.user = new User();
 }
+
 signup(user:User)
 {
  alert("New user created");
 }
+
 }
