a3af86cd413ccd125c81f5accc4d80fec18f7bdd
cevans0971 <cevans742@gmail.com>
2016-11-30 21:30:06 -0500

    working on view

--- app/pages/profileview/profileview.component.ts
+++ app/pages/profileview/profileview.component.ts
@@ -2,25 +2,7 @@ import { Component } from "@angular/core";
 import { User } from '../../shared/user/user'
 
 @Component({
-  selector: "profileview",
-  template:`
-<ScrollView>
- "<Label text='Profile View Component'></Label>"
-  <h1>text={{user.firstName+" " +user.lastName}}</h1>
-<div>
-  <p class="leftie">Name <br/> Address <br/> Recent Activity<br/><\p> 
-</div>
-
-<div>
-"<Label text={{user.firstName <br/> user.lastName}}></Label>"
-"<Label text={{user.address <br/> user.city <br/> user.country <br/> user.zip}}></Label>"
-
-</div>
-
-</Scrollview>
-`,
-   styleUrls: ["./profileview.component.css"]
+  selector: "profile-shuttle-history",
+  template: "<Label text='Profile View Component'></Label>"
 })
-export class ProfileViewComponent {
-user:User;
-}
+export class ProfileViewComponent {}
