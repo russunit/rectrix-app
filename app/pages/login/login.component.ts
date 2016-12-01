81e03e6d93de1949544413565296f39fe89ffe98
russunit <russunit@gmail.com>
2016-12-01 16:29:43 -0500

    more work on user scheme

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,7 +1,53 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
 
 @Component({
   selector: "login",
-  template: "<Label text='Login Component'></Label>"
+  template: `
+  	<label text='Sign in to Rectrix' horizontalAlignment='center'></label>
+
+	<label text='user name'></label>
+	<TextField  autocapitalizationType="none" [(ngModel)]="user.username"></TextField>
+
+	<label text='password'></label>
+	<TextField autocapitalizationType="none" secure="true" [(ngModel)]="user.password"></TextField>
+
+	<Button text="Sign in" (tap)="login()" horizontalAlignment='center'></Button>
+	`,
+	providers: [UserService],
 })
-export class LoginComponent {}
+export class LoginComponent
+{
+	user: User;
+
+	constructor(private router: Router, private userService: UserService) 
+	{
+    	this.user = new User(); 
+		this.user.firstName= "";
+		this.user.lastName= "";
+		this.user.address= "";
+		this.user.city= "";
+		this.user.country= "";
+		this.user.zip= "";
+		this.user.username= "";
+		this.user.password= "";
+		this.user.email= "";
+		this.user.charterHistory= null;
+		this.user.shuttleHistory= null;
+	}
+
+	login() {
+    this.userService.login(this.user)
+      .subscribe(
+        () => alert("Signed in as "+this.user.username+"!"),
+        (error) => alert("Unfortunately we could not find your account.")
+      );
+  }
+
+
+  	
+}
