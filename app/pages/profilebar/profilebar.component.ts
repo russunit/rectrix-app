e4f2c0d312dc576255fd34f9a8b498f4cd206fef
russunit <russunit@gmail.com>
2016-12-03 17:25:49 -0500

    work on profilebar

--- app/pages/profilebar/profilebar.component.ts
+++ app/pages/profilebar/profilebar.component.ts
@@ -1,13 +1,20 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router"; 
 import { Location } from '@angular/common';
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
 
 @Component({
   selector: "profilebar",
   template: ` 
   	<StackLayout orientation="horizontal" horizontalAlignment="center">
-  		<button text="Log In" (tap)="logIn()" height="40" width={{buttonW}}></button>
-  		<button text="Sign Up" (tap)="signUp()" height="40" width={{buttonW}}></button> 
+
+
+  		
+      <button text={{button1}} (tap)="logIn()" height="40" width={{buttonW}}></button>
+      <button text={{button2}} (tap)="signUp()" height="40" width={{buttonW}}></button> 
+
+
   	</StackLayout>
   	`,
   styleUrls: ["pages/profilebar/profilebar-common.css"]
@@ -19,16 +26,54 @@ export class ProfilebarComponent
     width: number = this.screen.mainScreen.widthDIPs;
     buttonW: number = this.width * .45;
 
+    currentUser: User;
+    loggedIn: boolean;
+
+    button1: string;
+    button2: string;
+
 	constructor(private router: Router, private location: Location)
-	{}
+	{
+    this.currentUser = null;
+    this.loggedIn = false;
+    this.button1 = "Log In";
+    this.button2 = "Sign Up";
+  }
 
 	logIn()
 	{
-		this.router.navigate(["/login"]);
+    if(!this.loggedIn)
+    {
+      this.router.navigate(["/login"]);
+    }
 	}
 
 	signUp()
 	{
-		this.router.navigate(["/signup"]);
+    if(!this.loggedIn)
+    {
+      this.router.navigate(["/signup"]);
+    }
+
 	}
+
+  signIn(user: User)
+  {
+    this.currentUser = user;
+    this.loggedIn = false;
+
+    this.button1 = "Hello, "+user.username;
+    this.button2 = "Sign Up";
+  }
+
+  signOut()
+  {
+    this.currentUser = null;
+    this.loggedIn = false;
+
+    this.button1 = "Log In";
+    this.button2 = "Sign Up";
+  }
+
+
 }
