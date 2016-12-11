252aa5176723a4568dd1d9d10b047158c0945f1c
russunit <russunit@gmail.com>
2016-12-11 16:45:16 -0500

    getting ready to use SkyVantage API...

--- app/shared/config.js
+++ app/shared/config.js
@@ -2,6 +2,7 @@
 class Config {
 }
 Config.apiUrl = "https://api.everlive.com/v1/GWfRtXi1Lwt4jcqK/";
+//static apiUrl = "http://test.api.skyvantage.com/"
 Config.token = "";
 exports.Config = Config;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTtBQUdBLENBQUM7QUFGUSxhQUFNLEdBQUcsK0NBQStDLENBQUM7QUFDekQsWUFBSyxHQUFHLEVBQUUsQ0FBQztBQUZQLGNBQU0sU0FHbEIsQ0FBQSJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTtBQUlBLENBQUM7QUFIUSxhQUFNLEdBQUcsK0NBQStDLENBQUM7QUFDaEUsbURBQW1EO0FBQzVDLFlBQUssR0FBRyxFQUFFLENBQUM7QUFIUCxjQUFNLFNBSWxCLENBQUEifQ==
