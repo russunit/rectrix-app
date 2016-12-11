3190daec153363237250c1bb8ea75ce4afa74c82
russunit <russunit@gmail.com>
2016-12-11 16:45:38 -0500

    missing ;

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,5 +1,5 @@
 export class Config {
   static apiUrl = "https://api.everlive.com/v1/GWfRtXi1Lwt4jcqK/";
-  //static apiUrl = "http://test.api.skyvantage.com/"
+  //static apiUrl = "http://test.api.skyvantage.com/";
   static token = "";
 }
