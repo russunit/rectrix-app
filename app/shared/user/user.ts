13c1e5bc52f38dbea5e9f01c087b753d893745c7
russunit <russunit@gmail.com>
2016-12-15 21:30:07 -0500

    fixed bug with object arrays

--- app/shared/user/user.ts
+++ app/shared/user/user.ts
@@ -3,15 +3,21 @@ import { ShuttleRequest } from "../shuttle-request/shuttle-request";
 
 export class User 
 {
-firstName: string;
-lastName: string;
-address: string;
-city: string;
-country: string;
-zip: string;
-username: string;
-password: string;
-email: string;
-charterHistory: CharterRequest[];
-shuttleHistory: ShuttleRequest[];
+	firstName: string;
+	lastName: string;
+	address: string;
+	city: string;
+	country: string;
+	zip: string;
+	username: string;
+	password: string;
+	email: string;
+	charterHistory: Array<CharterRequest>;
+	shuttleHistory: Array<ShuttleRequest>;
+
+	constructor()
+	{
+		this.charterHistory = new Array<CharterRequest>();
+		this.shuttleHistory = new Array<ShuttleRequest>();
+	}
 }
