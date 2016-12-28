7b49a5a48f49087e3ce055e76d5ab8c61d2a7798
cevans0971 <cevans742@gmail.com>
2016-12-28 00:38:38 -0500

    Fixed label visibility on profile view

--- app/pages/profileview/profileview.component.ts
+++ app/pages/profileview/profileview.component.ts
@@ -10,12 +10,12 @@ import {Subscription} from 'rxjs/Subscription';
   template: `
     <ScrollView>
       <StackLayout>
-        <Label text ="{{currentUser.username}}" class="header" horizontalAlignment="center"></Label> 
+        <Label text ={{currentUser.username}} class="header" horizontalAlignment="center"></Label> 
         <GridLayout rows="auto,auto" columns="auto,auto">
           <Label text ="Name" row="0" col="0" ></Label>
           <Label text = "Address" row="1" col="0"></Label>
-          <Label text = "{{currentUser.firstName + ' ' + currentUser.lastName}}" row="0" col="1"></Label>
-          <Label text = "{{currentUser.address + ' ' + currentUser.city + ' ' + currentUser.country + ' ' + currentUser.zip }}" row="1" col="1"></Label>
+          <Label text = {{this.fullName}} row="0" col="1"></Label>
+          <Label text = {{this.addresses}} row="1" col="1"></Label>
         </GridLayout>
         <Button text="Edit Profile" (tap)="makeChanges()" height="{{buttonH}}" width="{{buttonW}}" horizontalAlignment='center'></Button>
       </StackLayout>
@@ -29,7 +29,9 @@ export class ProfileViewComponent implements OnInit {
 currentUser: User;
  
     loggedIn: boolean;
+    addresses: string;
 
+    fullName: string; 
     subscription1:Subscription;
     subscription2:Subscription;
 
@@ -44,10 +46,10 @@ screen = this.platform.screen;
 constructor(private router: Router, private userService: UserService, private currentUserService: CurrentUserService){}
 ngOnInit() {
         
-
                this.subscription1 = this.currentUserService.loggedIn$.subscribe(loggedIn => this.loggedIn = loggedIn );
                this.subscription2 = this.currentUserService.currentUser$.subscribe(currentUser => this.currentUser = currentUser );
-
+this.addresses = this.currentUser.address + " " + this.currentUser.city + " " + this.currentUser.country + " " + this.currentUser.zip;
+this.fullName = this.currentUser.firstName + " " + this.currentUser.lastName;
                if(!this.loggedIn)
                {
                 alert("Profile view unavailable. Sign in first.");
