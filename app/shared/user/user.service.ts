c952f94d37075423baf58b2c7e7c90f69fd35b02
russunit <russunit@gmail.com>
2017-02-08 01:42:01 -0500

    converted my method to working status for typescript

--- app/shared/user/user.service.ts
+++ app/shared/user/user.service.ts
@@ -12,6 +12,12 @@ import { ShuttleRequest } from "../shuttle-request/shuttle-request";
 
 @Injectable()
 export class UserService {
+
+  charterHistorySize: number;
+  shuttleHistorySize: number;
+  outString: string;
+
+
 	constructor(private http: Http) {}
 
 	register(user: User) {
@@ -65,60 +71,56 @@ export class UserService {
   //converts whole profile to string for our http server
   userProfileToString(user: User) 
   {
-    charterHistorySize: number;
-    shuttleHistorySize: number;
-    outString: string;
+    this.charterHistorySize = user.charterHistory.length;
+    this.shuttleHistorySize = user.shuttleHistory.length;
+    this.outString = "";
 
-    charterHistorySize = user.charterHistory().size();
-    shuttleHistorySize = user.shuttleHistory().size();
-    outString = "";
-
-    outString += user.firstName + "$";
-    outString += user.lastName + "$";
-    outString += user.address + "$";
-    outString += user.city + "$";
-    outString += user.country + "$";
-    outString += user.zip + "$";
-    outString += user.username + "$";
-    outString += user.password + "$";
-    outString += user.email + "$";
+    this.outString += user.firstName + "$";
+    this.outString += user.lastName + "$";
+    this.outString += user.address + "$";
+    this.outString += user.city + "$";
+    this.outString += user.country + "$";
+    this.outString += user.zip + "$";
+    this.outString += user.username + "$";
+    this.outString += user.password + "$";
+    this.outString += user.email + "$";
     
-    outString += charterHistorySize + "$";
-    for(int x = 0; x < charterHistorySize; x++)
+    this.outString += this.charterHistorySize + "$";
+    for(let charterRequest of user.charterHistory)
     {
-      outString += user.charterHistory.get(x).firstName + "$";
-      outString += user.charterHistory.get(x).lastName + "$";
-      outString += user.charterHistory.get(x).phoneNumber + "$";
-      outString += user.charterHistory.get(x).tripType + "$";
-      outString += user.charterHistory.get(x).departLocation + "$";
-      outString += user.charterHistory.get(x).departDate + "$";
-      outString += user.charterHistory.get(x).departTime + "$";
-      outString += user.charterHistory.get(x).arriveLocation + "$";
-      outString += user.charterHistory.get(x).arriveDate + "$";
-      outString += user.charterHistory.get(x).arriveTime + "$";
-      outString += user.charterHistory.get(x).requirements + "$";
-      outString += user.charterHistory.get(x).preferredCraft + "$";
+      this.outString += charterRequest.firstName + "$";
+      this.outString += charterRequest.lastName + "$";
+      this.outString += charterRequest.phoneNumber + "$";
+      this.outString += charterRequest.tripType + "$";
+      this.outString += charterRequest.departLocation + "$";
+      this.outString += charterRequest.departDate + "$";
+      this.outString += charterRequest.departTime + "$";
+      this.outString += charterRequest.arriveLocation + "$";
+      this.outString += charterRequest.arriveDate + "$";
+      this.outString += charterRequest.arriveTime + "$";
+      this.outString += charterRequest.requirements + "$";
+      this.outString += charterRequest.preferredCraft + "$";
     }
 
-    outString += shuttleHistorySize + "$";
-    for(int x = 0; x < shuttleHistorySize; x++)
+    this.outString += this.shuttleHistorySize + "$";
+    for(let shuttleRequest of user.shuttleHistory)
     {
-      outString += user.shuttleHistory.get(x).firstName + "$";
-      outString += user.shuttleHistory.get(x).lastName + "$";
-      outString += user.shuttleHistory.get(x).phoneNumber + "$";
-      outString += user.shuttleHistory.get(x).tripType + "$";
-      outString += user.shuttleHistory.get(x).departLocation + "$";
-      outString += user.shuttleHistory.get(x).departDate + "$";
-      outString += user.shuttleHistory.get(x).departTime + "$";
-      outString += user.shuttleHistory.get(x).arriveLocation + "$";
-      outString += user.shuttleHistory.get(x).arriveDate + "$";
-      outString += user.shuttleHistory.get(x).arriveTime + "$";
-      outString += user.shuttleHistory.get(x).numAdults + "$";
-      outString += user.shuttleHistory.get(x).numChildren + "$";
-      outString += user.shuttleHistory.get(x).numInfants + "$";
+      this.outString += shuttleRequest.firstName + "$";
+      this.outString += shuttleRequest.lastName + "$";
+      this.outString += shuttleRequest.phoneNumber + "$";
+      this.outString += shuttleRequest.tripType + "$";
+      this.outString += shuttleRequest.departLocation + "$";
+      this.outString += shuttleRequest.departDate + "$";
+      this.outString += shuttleRequest.departTime + "$";
+      this.outString += shuttleRequest.arriveLocation + "$";
+      this.outString += shuttleRequest.arriveDate + "$";
+      this.outString += shuttleRequest.arriveTime + "$";
+      this.outString += shuttleRequest.numAdults + "$";
+      this.outString += shuttleRequest.numChildren + "$";
+      this.outString += shuttleRequest.numInfants + "$";
     }
     
-    return outString;
+    return this.outString;
 }
 
 
