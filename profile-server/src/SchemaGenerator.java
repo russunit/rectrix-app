a8bce9e13d1a493ad892afd3e080f49903593e1e
russunit <russunit@gmail.com>
2017-03-24 20:48:19 -0400

    worded on schema generator, realized this is a huge obstacle
    there is a ton of airline jargon and required info we don't have, such
    as payment, etc. We really need access to the Rectrix website's server
    which does this work, as it is (in my opinion) beyond the scope of what
    can be done in time.

--- profile-server/src/SchemaGenerator.java
+++ profile-server/src/SchemaGenerator.java
@@ -6,25 +6,162 @@
 
 /**
  *
- * @author Chris
+ * @author Chris, Malcolm
  */
-public class SchemaGenerator {
-    private String original;
-    private String generatedXML;
+public class SchemaGenerator 
+{
     public SchemaGenerator(String original)
+    { }
+    
+    public String makeAccountsAvailabilityRQ(String airlineCode, String accountNumber, String accountName)
     {
-        this.original = original;
-        //original is the string that we get from the http specifying what the server is supposed to do, We want to generate this into XNL
-        generateSchema();
-    }
-
-    private void generateSchema() {
-        //code that will generate the xml schema to communicate with skyvantage
-        
-    }
-    public String getXML()
-    {
-        return generatedXML;
+    	String output = "";
+    	output += "<AccountsAvailabilityRQ>\n" +
+    			"	<AirlineCode>"+airlineCode+"</AirlineCode>\n" +
+    			"	<AccountNumber>"+accountNumber+"</AccountNumber>\n" +
+    			"	<AccountName>"+accountName+"</AccountName>\n" +
+    			"</AccountsAvailabilityRQ>\n";
+"
+    	return output;
     }
     
+    public String makeAirAvailabilityRQ(String airlineCode, String arrivalCode1, String departureCode1, String departureDate1, String flightNumber1,
+    		String arrivalCode2, String departureCode2, String departureDate2, String flightNumber2)
+    {
+    	String output = "";
+    	output += 	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
+					"<AirAvailabilityRQ>\n"+
+					"	<AirlineCode>"+airlineCode+"</AirlineCode>\n"+
+					"	<OriginDestination ArrivalCode=\""+arrivalCode1+"\" DepartureCode=\""+departureCode1 +"\"\n"+
+					"		<Routes>\n"+
+					"			<Route>\n"+
+					"				<DepartureDate>"+departureDate1+"</DepartureDate>\n"+
+					"				<FlightNumbers>\n"+
+					"					<FlightNumber>"+flightNumber1+"</FlightNumber>\n"+
+					"				</FlightNumbers>\n"+
+					"			</Route>\n"+
+					"		</Routes>\n"+
+					"	</OriginDestination>\n"+
+					"	<OriginDestination ArrivalCode=\""+arrivalCode2+"\" DepartureCode=\""+departureCode2+"\">\n"+
+					"		<Routes>\n"+
+					"			<Route>\n"+
+					"				<DepartureDate>"+departureDate2+"</DepartureDate>\n"+
+					"				<FlightNumbers>\n"+
+					"					<FlightNumber>"+flightNumber2+"</FlightNumber>\n"+
+					"				</FlightNumbers>\n"+
+					"			</Route>\n"+
+					"		</Routes>\n"+
+					"	</OriginDestination>\n"+
+					"</AirAvailabilityRQ>\n";
+"
+    	return output;
+    }    
+    
+    public String makeCityPairRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makeFareClassRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePassengerFieldsRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePricingRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makeServiceListRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makeTimeTableRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrCreateRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrAddPaymentRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrAddSegmentRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrAddServiceRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrChangeRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrDisplayRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrSplitRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makePnrStatusChangeRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makeAvailablePayTypesRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makeAircraftSeatsRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makeInfantPricingRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
+    
+    public String makeSeatAvailabilityStatusRQ(String input)
+    {
+    	String output = "";
+    	return output;
+    }
 }
